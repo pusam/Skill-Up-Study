d2b470c9596c33d5023ee89fe85f50024852814a
t0200145 <t0200145@ktng.com>
2022-03-25 15:46:03 +0900

    없는 숫자 더하기

---
+++ src/main/java/NoNumberAdd.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoNumberAdd
+{
+        /*
+        0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. numbers에서 찾을 수 없는
+         0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+
+         입출력 예
+numbers	result
+[1,2,3,4,6,7,8,0]	14
+[5,8,4,0,6,7,9]	6
+         */
+
+    public int solution(int[] numbers)
+    {
+        int answer = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int number : numbers)
+            list.add(number);
+
+        for(int i =0; i<10; i++)
+            if(!list.contains(i))
+                answer += i;
+
+        return answer;
+    }
+
+    public static void main(String[] args)
+    {
+        NoNumberAdd noNumberAdd = new NoNumberAdd();
+        noNumberAdd.solution(new int[]{1, 2, 3, 4, 6, 7, 8, 0});
+    }
+}
